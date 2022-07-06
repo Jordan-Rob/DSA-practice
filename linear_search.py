4e67eca0581d65cc7ae9ee7c3b622bdb5002b913
Jordan <jordanrob709@gmail.com>
2022-07-06 03:30:33 +0300

    implemented linear-search, binary-search & recursive-binary-search

---
+++ linear_search.py
@@ -0,0 +1,26 @@
+from unittest import result
+
+
+def linear_search(list, target):
+    """
+    Return the index position of the target if found, else return None 
+    """
+    
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index:", index)
+    else:
+        print("Target not found in list")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers, 12)
+verify(result)
+
+result1 = linear_search(numbers, 4)
+verify(result1)
